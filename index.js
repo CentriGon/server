2711c09ca669814e4a370678e9c88abec95900c4
CentriGon <linholmtorbjorn@gmail.com>
2023-07-15 15:03:54 -0400

    p

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require("puppeteer")
 
 const { Server } = require("socket.io")
 
-app.use(cors)
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
